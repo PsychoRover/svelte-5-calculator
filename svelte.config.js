acfe9a2e7c17813753b060812bd9b29ec6fa3e59
PsychoRover <gtorrent12@gmail.com>
2024-10-25 14:12:37 +0300

    chore: Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -16,12 +16,12 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: '404.html',
 			precompress: false,
 			strict: true
 		}),
 		paths: {
-			base: '/svelte-5-calculator'
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		},
 		alias: {
 			$calc: './src/lib/calculator'
